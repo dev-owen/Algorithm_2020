3f5b182151ba1cfe92bae2d2000854dcca8c0243
Wonjong Oh <jgyuity1289@gmail.com>
2020-04-14 15:02:08 +0900

    백준 11438(LCA 2) 실패
    LCA 알고리즘

--- src/BOJ_11438.java
+++ src/BOJ_11438.java
@@ -4,36 +4,32 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ_11438 {
-	static List<Integer>[] list = new ArrayList[100001];
-	static int[] depth = new int[100001]; // 해당 노드의 깊이
-	static int[][] parent = new int[100001][21]; // k = parent[a][b] 인 경우 a의 2^b 번째 조상이 k이다.
+	static HashMap<Integer, ArrayList<Integer>> treeMap = new HashMap<>();
+	static int[] depth; // 해당 노드의 깊이
+	static int[][] parent; // k = parent[a][b] 인 경우 a의 2^b 번째 조상이 k이다.
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		list[1] = new ArrayList<>();
-		depth[1] = 1;
+		depth = new int[N+1];
+		parent = new int[N+1][18];
+		treeMap.put(1, new ArrayList<>());
+		depth[1] = 0;
 
 		for(int i = 1; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int firstNode = Integer.parseInt(st.nextToken());
 			int secondNode = Integer.parseInt(st.nextToken());
-			if(list[firstNode] == null) {
-				list[firstNode] = new ArrayList<>();
-				depth[firstNode] = depth[secondNode]+1;
-				parent[firstNode][0] = secondNode;
-				list[secondNode].add(firstNode);
+			if(!treeMap.containsKey(firstNode)) {
+				treeUpdate(secondNode, firstNode);
 			}
-			else if(list[secondNode] == null) {
-				list[secondNode] = new ArrayList<>();
-				depth[secondNode] = depth[firstNode]+1;
-				parent[secondNode][0] = firstNode;
-				list[firstNode].add(secondNode);
+			else if(!treeMap.containsKey(secondNode)) {
+				treeUpdate(firstNode, secondNode);
 			}
 		}
 
-		for(int i = 1; i <= 20; i++) { // 나머지 parent 채운다.
+		for(int i = 1; i <= 17; i++) { // 나머지 parent 채운다.
 			for(int j = 1; j <= N; j++) {
 				parent[j][i] = parent[parent[j][i-1]][i-1];
 			}
@@ -48,6 +44,16 @@ public class BOJ_11438 {
 			sb.append(lca(firstNode, secondNode) + "\n");
 		}
 		System.out.println(sb);
+		br.close();
+	}
+
+	static void treeUpdate(int parentNode, int childNode) {
+		treeMap.put(childNode, new ArrayList<>());
+		depth[childNode] = depth[parentNode]+1;
+		parent[childNode][0] = parentNode;
+		ArrayList<Integer> tmp = treeMap.get(parentNode);
+		tmp.add(childNode);
+		treeMap.replace(parentNode, tmp);
 	}
 
 
@@ -62,7 +68,7 @@ public class BOJ_11438 {
 			high = first;
 		}
 
-		for(int i = 20; i >= 0; i--) {
+		for(int i = 17; i >= 0; i--) {
 			int diff = depth[low] - depth[high];
 			if(Math.pow(2, i) <= diff) {
 				low = parent[low][i]; // low를 위로 끌어올린다.
@@ -72,7 +78,7 @@ public class BOJ_11438 {
 		if( low == high ) return high;
 
 		// 이제 같은 선(depth) 상에 있으므로 조상을 하나씩 찾아 나간다.
-		for(int i = 20; i >= 0; i--) {
+		for(int i = 17; i >= 0; i--) {
 			if(parent[low][i] != parent[high][i]) {
 				low = parent[low][i];
 				high = parent[high][i];
