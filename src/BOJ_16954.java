5a9b74cad2be7c3b178da674ce729f105cd38024
dev-owen <jgyuity1289@gmail.com>
2020-08-31 00:19:29 +0900

    백준 16954 / 움직이는 미로 탈출 / 성공
    BFS

--- src/BOJ_16954.java
+++ src/BOJ_16954.java
@@ -1,90 +1,67 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class BOJ_16954 {
-    static ArrayList<boolean[][]> aryList = new ArrayList<>();
-    static int[] dx = {1, 1, 0, -1, -1, -1, 0, 1}, dy = {0, 1, 1, 1, 0, -1, -1, -1}; // 0: 3시, 시계 반대방향... ex. 2: 12시, 4: 9시,
+    public static final int[] dx = {0, 1, 0, -1, 0, 1, -1, -1, 1};
+    public static final int[] dy = {0, 0, 1, 0, -1, 1, 1, -1, -1};
+    public static int ans;
+    public static char[][] map;
+    public static Queue<Pos> q;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[][] initArray = new boolean[8][8];
-        for (int i = 7; i >= 0; i--) {
-            char[] inputCharArray = br.readLine().toCharArray();
-            for (int j = 0; j <= 7; j++) {
-                initArray[i][j] = inputCharArray[j] != '#';
+        map = new char[8][8];
+        q = new LinkedList<>();
+        for (int i = 0; i < 8; i++) {
+            char[] ch = br.readLine().toCharArray();
+            for (int j = 0; j < 8; j++) {
+                map[i][j] = ch[j];
             }
         }
-        aryList.add(initArray);
-        for (int i = 1; i <= 7; i++) {
-            boolean[][] tmpArray = new boolean[8][8];
-            for (int j = 0; j <= 7; j++) {
-                for (int k = 0; k <= 7; k++) {
-                    tmpArray[j][k] = initArray[j][k];
-                }
-            }
-            boolean[][] movedArray = arrayMoveWithTime(tmpArray, i);
-            aryList.add(movedArray);
-        }
+        q.add(new Pos(7, 0));
+        ans = (bfs()) ? 1 : 0;
+        System.out.println(ans);
+    }
 
-        Point initPoint = new Point(0, 0, 0, -1);
-        LinkedList<Point> queue = new LinkedList<>();
-        queue.add(initPoint);
-        boolean flag = false;
-        while (!queue.isEmpty()) {
-            Point p = queue.pop();
-            int nowX = p.x, nowY = p.y, nowT = p.time, exDir = p.dir;
-            if (nowX == 7 && nowY == 7) {
-                flag = true;
-                break;
-            }
-            for (int i = 0; i <= 7; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-                int nextT = nowT + 1;
-                int nowDir = i;
-                if (nextX >= 0 && nextX <= 7 && nextY >= 0 && nextY <= 7 && nowDir + exDir != 7) {
-                    if (nowT <= 7) {
-                        if (aryList.get(nowT)[nextX][nextY]) {
-                            if (nextT <= 7) {
-                                if (aryList.get(nextT)[nextX][nextY]) queue.add(new Point(nextX, nextY, nextT, nowDir));
-                            } else queue.add(new Point(nextX, nextY, nextT, nowDir));
-                        }
-                    } else {
-                        if (nextT <= 7) {
-                            if (aryList.get(nextT)[nextX][nextY]) queue.add(new Point(nextX, nextY, nextT, nowDir));
-                        } else queue.add(new Point(nextX, nextY, nextT, nowDir));
+    private static boolean bfs() {
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int t = 0; t < size; t++) {
+                Pos pos = q.poll();
+                if (map[pos.row][pos.col] == '#') continue;
+                for (int i = 0; i < 9; i++) {
+                    int nx = pos.row + dx[i], ny = pos.col + dy[i];
+                    if (nx < 0 || nx >= 8 || ny < 0 || ny >= 8) continue;
+                    if (nx == 0 && ny == 7) {
+                        return true;
+                    }
+                    if (map[nx][ny] != '#') {
+                        q.add(new Pos(nx, ny));
                     }
                 }
             }
+            down();
         }
-        System.out.println(flag ? 1 : 0);
+        return false;
     }
 
-    private static boolean[][] arrayMoveWithTime(boolean[][] arr, int time) {
-        boolean[][] resArray = new boolean[8][8];
-        for (int i = 0; i <= 7; i++) {
-            for (int j = 0; j <= 7; j++) {
-                if (i + time <= 7) {
-                    if (!arr[i + time][j]) resArray[i][j] = false;
-                    else resArray[i][j] = true;
-                } else resArray[i][j] = true;
+    private static void down() {
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 7; j >= 0; j--) {
+                if (i - 1 < 0) map[i][j] = '.';
+                else map[i][j] = map[i - 1][j];
             }
         }
-        return resArray;
     }
 
-    private static class Point {
-        int x, y, time, dir;
+    private static class Pos {
+        int row, col;
 
-        private Point(int x, int y, int time, int dir) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-            this.dir = dir;
+        public Pos(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
     }
 }
-
