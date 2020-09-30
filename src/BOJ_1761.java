14370e0dcfe78fcfd2c395a43572db9639846d53
dev-owen <jgyuity1289@gmail.com>
2020-10-01 01:29:11 +0900

    백준 1761 / 정점들의 거리 / 플레 5
    LCA, DFS

--- src/BOJ_1761.java
+++ src/BOJ_1761.java
@@ -1,78 +1,118 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class BOJ_1761 {
-	static int[] depth = new int[40001], parent = new int[40001], distFromRoot = new int[40001];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
 
-		// 처음 받는 노드를 루트로 잡아서 거기서부터의 길이를 계산
-		st = new StringTokenizer(br.readLine());
-		int rootIdx = Integer.parseInt(st.nextToken());
-		int otherIdx = Integer.parseInt(st.nextToken());
-		parent[otherIdx] = rootIdx;
-		depth[rootIdx] = 1;
-		distFromRoot[otherIdx] = Integer.parseInt(st.nextToken());
-		for(int i = 1; i < N-1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int firstIdx = Integer.parseInt(st.nextToken());
-			int secondIdx = Integer.parseInt(st.nextToken());
-			int dist = Integer.parseInt(st.nextToken());
-			if(distFromRoot[firstIdx] > 0) {
-				parent[secondIdx] = firstIdx;
-				depth[secondIdx] = depth[firstIdx]+1;
-				distFromRoot[secondIdx] = distFromRoot[firstIdx]+dist;
-			}
-			else if(distFromRoot[secondIdx] > 0) {
-				parent[firstIdx] = secondIdx;
-				depth[firstIdx] = depth[secondIdx]+1;
-				distFromRoot[firstIdx] = distFromRoot[secondIdx]+dist;
-			}
-			else { // rootIdx에서 트리 생성해야 함
-				if(firstIdx == rootIdx) {
-					parent[secondIdx] = firstIdx;
-					depth[secondIdx] = 1;
-					distFromRoot[secondIdx] = dist;
-				} else {
-					parent[firstIdx] = secondIdx;
-					depth[firstIdx] = 1;
-					distFromRoot[firstIdx] = dist;
-				}
-			}
-		}
-		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int firstNode = Integer.parseInt(st.nextToken());
-			int secondNode = Integer.parseInt(st.nextToken());
-			bw.write(distFromRoot[firstNode] + distFromRoot[secondNode] - 2 * distFromRoot[lca(firstNode, secondNode)] + "\n");
-		}
-		bw.flush();
-		bw.close();
-	}
+    static ArrayList<Integer>[] list;
+    static ArrayList<Integer>[] dlist;
 
-	static int lca(int firstIdx, int secondIdx) { // 두 노드의 최소 공통 노드 idx를 찾아서 반환
-		int diff = depth[firstIdx] - depth[secondIdx];
+    static int[] depth;
+    static int[][] parent;
+    static int[] distance;
+    static boolean[] visited;
 
-		if(diff < 0) {
-			while (diff != 0) {
-				diff++;
-				secondIdx = parent[secondIdx];
-			}
-		} else {
-			while (diff != 0) {
-				diff++;
-				firstIdx = parent[firstIdx];
-			}
-		}
-		while (firstIdx != secondIdx) {
-			firstIdx = parent[firstIdx];
-			secondIdx = parent[secondIdx];
-		}
-		return firstIdx;
-	}
+    static int N;
+    static int K;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        list = new ArrayList[N + 1];
+        dlist = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<Integer>();
+            dlist[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 1; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+            dlist[a].add(c);
+            dlist[b].add(c);
+        }
+
+        depth = new int[N + 1];
+        distance = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        int temp = 1;
+        K = 0;
+
+        while (temp <= N) {
+            temp <<= 1;
+            K++;
+        }
+
+        parent = new int[N + 1][K];
+        dfs(1, 0, 0);
+        fillParent();
+        int Q = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= Q; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long answer = getLCA(a, b);
+            System.out.println(answer);
+        }
+    }
+
+    private static void fillParent() {
+        for (int i = 1; i < K; i++) {
+            for (int j = 1; j <= N; j++) {
+                parent[j][i] = parent[parent[j][i - 1]][i - 1];
+            }
+        }
+    }
+
+    private static long getLCA(int a, int b) {
+        if (depth[a] < depth[b]) {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        int orgA = a;
+        int orgB = b;
+
+        for (int i = K - 1; i >= 0; i--) {
+            int diff = depth[a] - depth[b];
+            if ((diff & (1 << i)) != 0) {
+                a = parent[a][i];
+            }
+        }
+
+        if (a == b) return distance[orgA] - distance[orgB];
+
+        for (int i = K - 1; i >= 0; i--) {
+            if (parent[a][i] != parent[b][i]) {
+                a = parent[a][i];
+                b = parent[b][i];
+            }
+        }
+        int lca = parent[a][0];
+        long answer = distance[orgA] + distance[orgB] - (distance[lca] * 2);
+        return answer;
+    }
+
+    private static void dfs(int node, int dep, int dis) {
+        visited[node] = true;
+        depth[node] = dep;
+        distance[node] = dis;
+
+        for (int i = 0; i < list[node].size(); i++) {
+            if (!visited[list[node].get(i)]) {
+                parent[list[node].get(i)][0] = node;
+                dfs(list[node].get(i), dep + 1, dis + dlist[node].get(i));
+            }
+        }
+    }
 }
